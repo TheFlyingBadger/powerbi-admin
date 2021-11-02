11e995e92d0176f5f0506d692639fd0c260a943e
Jon Mills <jon@badger.shoes>
2021-11-02 14:09:09 +0800

    more

--- app/__main__.py
+++ app/__main__.py
@@ -10,11 +10,38 @@ if __name__ == '__main__':
     # ic(CONFIG.access_token.expires_on)
     # ic(CONFIG.access_token.token)
     # ic(CONFIG.session)
-    ic(CONFIG.client.__dict__.keys())
+    # ic(CONFIG.__dict__.keys())
     # ic(CONFIG.client.__dict__['config'].__dict__)
     # ic(CONFIG.client.__dict__.keys())
     # ic(CONFIG.client._deserialize())
 
-    https: // api.powerbi.com/v1.0/myorg/admin/groups?$top = {$top}
-    # ops = CONFIG.client.get_available_operations()
+    # ic(type(CONFIG.client_secret_credential))
+    # ic(CONFIG.client_secret_credential)
+    # ic(CONFIG.client_secret_credential.__dict__)
+#     token = CONFIG.client_secret_credential.get_token(
+#         ["https://analysis.windows.net/powerbi/api/.default"])
+#     ic(type(token))
+#     ic(token)
+#     ic(str(token))
+#     ic(token.__dict__)
+#     ic(token.__dir__())
+#     raise Exception
+# # --------------------------------------------------
+# Check if a token was obtained, grab it and call the
+# Power BI REST API, otherwise throw up the error message
+# --------------------------------------------------
+    # access_token = result['access_token']
+    url_groups = 'https://api.powerbi.com/v1.0/myorg/groups'
+    url_groups = 'https://api.powerbi.com/v1.0/myorg/capacities'
+
+    header = {'Content-Type': 'application/json',
+              'Authorization': f'Bearer {CONFIG.access_token}',
+              'Host': 'api.powerbi.com'
+              }
+    api_out = requests.get(url=url_groups, headers=header)
+    ic(type(api_out))
+    ic(api_out)
+    ic(api_out.__dict__)
+    # print(api_out.json())
+    # # ops = CONFIG.client.get_available_operations()
     # ic(ops.__dict__)
--- app/functions.py
+++ app/functions.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from datetime import datetime
+from time import strftime
 
 try:
     from icecream import ic
@@ -37,19 +38,52 @@ except AttributeError:
 
 
 class azureClientSecretConnection (object):
+
+    def __repr__(self):
+        return f"azureClientSecretConnection(\'{self.yaml_file}\')"
+
+    def as_json(self, indent: int = 2):
+        import json
+        d = self.as_dict()
+        print(d)
+        return json.dumps(d, indent=indent)
+
+    def as_dict(self):
+        asDict = self.__dict__
+        d = {}
+        for k in (k for k in asDict if not k[0] == "_"):
+            val = asDict[k]
+            ic(k, type(val))
+            if type(val) == Path:
+                val = str(val)
+            elif type(val) == datetime:
+                val = val.strftime('%Y-%m-%d %H:%M:%S')
+            if isinstance(val, int | str | dict | list):
+                d[k] = val
+        return d
+
+    def __str__(self):
+        return str(self.as_dict())
+
     def __init__(self, configYAML: str | Path):
         from configFileHelper import Config
         if isinstance(configYAML, str):
             configYAML = Path(configYAML).resolve()
         config = Config(file_path=configYAML)
 
+        self._yaml = configYAML
         self._debug = config.get_bool("APP/DEBUG")
 
         from azure.identity import ClientSecretCredential
 
-        self._tenant_id = config.get('AZURE/TENANT_ID')
-        client_id = config.get('AZURE/CLIENT_ID')
-        scope = config.get('AZURE/SCOPE')
+        self.tenant_id = config.get('AZURE/TENANT_ID')
+        self.client_id = config.get('AZURE/CLIENT_ID')
+        self.scope = config.get('AZURE/SCOPE')
+        print(type(self.scope))
+        if isinstance(self.scope, list):
+            self.scope = ','.join(self.scope)
+            print(type(self.scope))
+        print(self.scope)
 
         try:
             clientSecretFile = Path(config.get('AZURE/CLIENT_SECRET_FILE'))
@@ -59,35 +93,36 @@ class azureClientSecretConnection (object):
             if not clientSecretFile.is_file():
                 raise FileNotFoundError
 
-            client_secret = clientSecretFile.read_text()
+            self._client_secret = clientSecretFile.read_text()
 
         except KeyError:
-            client_secret = config.get('AZURE/CLIENT_SECRET')
+            self._client_secret = config.get('AZURE/CLIENT_SECRET')
 
-        self.client_secret_credential = ClientSecretCredential(
-            tenant_id=self._tenant_id, client_id=client_id, client_secret=client_secret)
-        self.access_token = self.client_secret_credential.get_token(scope)
+        self._ClientSecretCredential = ClientSecretCredential(
+            tenant_id=self.tenant_id, client_id=self.client_id, client_secret=self._client_secret)
+        self._AccessToken = self._ClientSecretCredential.get_token(
+            self.scope, tenant_id=self.tenant_id)
+        self.access_token = self._AccessToken.token
+        self.token_expires_on = datetime.fromtimestamp(
+            self._AccessToken.expires_on)
 
-        from azure.mgmt.powerbiembedded.power_bi_embedded_management_client import PowerBIEmbeddedManagementClient
+        # Temp
+        self.client_secret = self._client_secret
 
-        self.client = PowerBIEmbeddedManagementClient(
-            credentials=self.client_secret_credential, subscription_id=self.subscription_id, base_url=None)
-
-    @property
+    @ property
     def debug(self):
         return self._debug
 
-    @property
-    def tenant_id(self):
-        return self._tenant_id
+    @ property
+    def yaml_file(self):
+        return self._yaml
 
-    @property
+    @ property
     def subscription_id(self):
-        return self._tenant_id
+        return self.tenant_id
 
 
 def getConfig(configFile: str = "config.yaml"):
 
     config = azureClientSecretConnection(configYAML=configFile)
-    ic_set(config.debug)
     return config
